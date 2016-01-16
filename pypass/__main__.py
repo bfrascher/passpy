540c33506db480df2d80ae1c722a7fe2cb8f5489
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen@gmail.com>
2016-01-16 17:48:37 +0100

    main: (Re-)Implement most functions and add doc

--- pypass/__main__.py
+++ pypass/__main__.py
@@ -12,6 +12,8 @@ class PassGroup(click.Group):
             cmd_name = 'ls'
         elif cmd_name == 'search':
             cmd_name = 'find'
+        elif cmd_name == 'gen':
+            cmd_name = 'generate'
         elif cmd_name == 'add':
             cmd_name = 'insert'
         elif cmd_name == 'remove' or cmd_name == 'delete':
@@ -20,14 +22,9 @@ class PassGroup(click.Group):
             cmd_name = 'mv'
         elif cmd_name == 'copy':
             cmd_name = 'cp'
-        else:
-            # TODO(benedikt) Use current cmd_name as first parameter
-            cmd_name = 'show'
 
-        click.echo('Command: {}'.format(cmd_name))
-
-
-
+        # TODO(benedikt) Figure out how to make 'show' the dfault
+        # command and pass cmd_name as the first argument.
         rv = click.Group.get_command(self, ctx, cmd_name)
         if rv is not None:
             return rv
@@ -39,46 +36,81 @@ def cli(ctx):
     ctx.obj = Store()
 
 
-@cli.command()
-@click.option('-p', '--path', type=str)
-@click.argument('gpg_id', nargs=-1)
+@cli.command(options_metavar='[ --path,-p ]')
+@click.option('-p', '--path', type=str,
+              help='Only set the gpg-ids for the given subfolder.')
+@click.argument('gpg_ids', nargs=-1, metavar='gpg-id')
 @click.pass_context
-def init(ctx, path, gpg_id):
-    ctx.obj.init_store(list(gpg_id), path=path)
+def init(ctx, path, gpg_ids):
+    """Initialize new password storage and use `gpg-id` for encryption.
+    Selectively reencrypts existing passwords using new `gpg-id`.
+
+    """
+    ctx.obj.init_store(list(gpg_ids), path=path)
 
 
 @cli.command()
 @click.argument('subfolder', type=str)
 @click.pass_context
 def ls(ctx, subfolder):
-    # keys = ctx.obj.ls_dir(subfolder)
-    # for key in keys:
-    #     click.echo(entry)
-    pass
+    # TODO(benedikt) Generate pretty output
+    try:
+        for key in ctx.obj.iter_dir(subfolder):
+            click.echo(key)
+    # If subfolder is actually a key in the password store pass shows
+    # the contents of that key.
+    except FileNotFoundError:
+        show(ctx, subfolder, False)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
 
 
 @cli.command()
-@click.argument('search_string', type=str, nargs=-1)
+@click.argument('search_string', type=str, metavar='search-string')
 @click.pass_context
 def grep(ctx, search_string):
-    pass
+    try:
+        results = ctx.obj.search(search_string)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+        return
+
+    for key in results:
+        # TODO(benedikt) Color this line
+        click.echo(key + ':')
+        for line, match in results[key]:
+            # TODO(benedikt) Color the matched part of the line
+            click.echo(line)
 
 
 @cli.command()
-@click.argument('pass_names', type=str, nargs=-1)
+@click.argument('pass_names', type=str, nargs=-1, metavar='pass-name')
 @click.pass_context
 def find(ctx, pass_names):
-    keys = ctx.obj.find(list(pass_names))
+    try:
+        keys = ctx.obj.find(list(pass_names))
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+        return
+
+    # TODO(benedikt) Pretty up the output (tree-like?)
     for key in keys:
         click.echo(key)
 
 
-@cli.command()
-@click.option('-c', '--clip', is_flag=True)
-@click.argument('pass_name', type=str)
+@cli.command(options_metavar='[ --clip,-c ]')
+@click.option('-c', '--clip', is_flag=True,
+              help='Copy the password to the clipboard instead of '
+              'printing it to the command line.')
+@click.argument('pass_name', type=str, metavar='pass-name')
 @click.pass_context
 def show(ctx, pass_name, clip):
-    data = ctx.obj.get_key(pass_name)
+    try:
+        data = ctx.obj.get_key(pass_name)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+        return
+
     if clip:
         # TODO(benedikt) Copy to clipboard
         pass
@@ -86,66 +118,119 @@ def show(ctx, pass_name, clip):
         clip.echo(data)
 
 
-@cli.command()
-@click.option('-e', '--echo', is_flag=True)
-@click.option('-m', '--multiline', is_flag=True)
-@click.option('-f', '--force', is_flag=True)
-@click.argument('pass_name', type=str)
+@cli.command(options_metavar='[ --echo,-e | --multiline,-m ] [ --force,-f ]')
+@click.option('-e', '--echo', 'input_method', flag_value='echo',
+              help='Don\'t ask to repeat the password.')
+@click.option('-m', '--multiline', 'input_method', flag_value='multiline',
+              help='Allows entering multiple lines of text for the key.')
+@click.option('-f', '--force', is_flag=True,
+              help='Any existing key at pass-name will be '
+              'silently overwritten.')
+@click.argument('pass_name', type=str, metavar='pass-name')
 @click.pass_context
-def insert(ctx, pass_name, echo, multiline, force):
-    if multiline:
-        pass
-        # TODO(benedikt) implement
-        # data = click.prompt('Enter contents of {} and press Ctrl+D when finished:'
-        #                     .format(pass_name), type=str)
+def insert(ctx, pass_name, input_method, force):
+    if input_method is None:
+        input_method = 'neither'
+    if input_method == 'multiline':
+        click.echo('Enter contents of {} and press Ctrl+D on an empty '
+                    'line when finished:'.format(pass_name))
+        lines = []
+        while True:
+            try:
+                line = input('> ')
+                lines.append(line)
+            except EOFError:
+                break
+        data = '\n'.join(lines)
     else:
+        echo = (input_method == 'echo')
         data = click.prompt('Enter password for {}: '.format(pass_name),
                             hide_input=True, confirmation_prompt=echo,
                             type=str)
-    ctx.obj.set_key(pass_name, data, force=force)
+
+    try:
+        ctx.obj.set_key(pass_name, data, force=force)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
 
 
 @cli.command()
-@click.argument('pass_name', type=str)
+@click.argument('pass_name', type=str, metavar='pass-name')
 @click.pass_context
 def edit(ctx, pass_name):
     pass
 
 
-@cli.command()
-@click.option('-n', '--no-symbols', is_flag=True)
-@click.option('-c', '--clip', is_flag=True)
-@click.option('-i', '--in-place', is_flag=True)
-@click.option('-f', '--force', is_flag=True)
-@click.argument('pass_name', type=str)
-@click.argument('pass_length', type=int)
+@cli.command(options_metavar='[ --no-symbols,-n ] [ --clip,-c ] '
+             '[ --in-place,-i ] [ --force,-f ]')
+@click.option('-n', '--no-symbols', is_flag=True,
+              help='If specified the password will only consist '
+              'of alphanumeric characters.')
+@click.option('-c', '--clip', is_flag=True,
+              help='Copy the password to the clipboard instead of '
+              'printing it on the command line.')
+@click.option('-i', '--in-place', is_flag=True,
+              help='Replace the first line of an existing key at '
+              'path-name with the newly generated password.')
+@click.option('-f', '--force', is_flag=True,
+              help='Overwrite an existing key at pass-name without '
+              'prompting the user first.')
+@click.argument('pass_name', type=str, metavar='pass-name')
+@click.argument('pass_length', type=int, metavar='pass-length')
 @click.pass_context
 def generate(ctx, pass_name, pass_length, no_symbols, clip, in_place, force):
-    pass
+    symbols = not no_symbols
+    try:
+        password = ctx.obj.gen_key(pass_name, pass_length, symbols, force, in_place)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+        return
+
+    if clip:
+        # TODO(benedikt) Copy password to the clipboard
+        pass
+    else:
+        click.echo(password)
 
 
-@cli.command()
-@click.option('-r', '--recursive', is_flag=True)
-@click.option('-f', '--force', is_flag=True)
-@click.argument('pass_name', type=str)
+@cli.command(options_metavar='[ --recursive,-r ] [ --force,-f ]')
+@click.option('-r', '--recursive', is_flag=True,
+              help='If pass-name is a directory, also remove all '
+              'it\'s contents.')
+@click.option('-f', '--force', is_flag=True,
+              help='Don\'t prompt for confirmation when removing a key.')
+@click.argument('pass_name', type=str, metavar='pass-name')
 @click.pass_context
 def rm(ctx, pass_name, recursive, force):
-    pass
+    # TODO(benedikt) Use force option when implemented
+    try:
+        ctx.obj.remove_path(pass_name, recursive)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+    except FileNotFoundError:
+        click.echo('{} is not in the password store.'.format(pass_name))
 
 
-@cli.command()
-@click.option('-f', '--force', is_flag=True)
-@click.argument('old_path', type=str)
-@click.argument('new_path', type=str)
+@cli.command(options_metavar='[ --force,-f ]')
+@click.option('-f', '--force', is_flag=True,
+              help='If specified existing files at `new-path` '
+              'will be silently overwritten.')
+@click.argument('old_path', type=str, metavar='old-path')
+@click.argument('new_path', type=str, metavar='old-path')
 @click.pass_context
 def mv(ctx, old_path, new_path, force):
-    pass
+    try:
+        ctx.obj.move_path(old_path, new_path, force)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+    except FileNotFoundError:
+        click.echo('{} is not in the password store.'.format(old_path))
 
 
-@cli.command(options_metavar='[ --force, -f ]')
+@cli.command(options_metavar='[ --force,-f ]')
 @click.option('-f', '--force', is_flag=True,
-              help='If specified existing files at `new-path` will be silently\
-              overwritten.')
+              help='If specified existing files at `new-path` '
+              'will be silently overwritten.')
 @click.argument('old_path', type=str, metavar='old-path')
 @click.argument('new_path', type=str, metavar='new-path')
 @click.pass_context
@@ -158,7 +243,19 @@ def cp(ctx, old_path, new_path, force):
     corresponding keys of their new destination.
 
     """
+    try:
+        ctx.obj.copy_path(old_path, new_path, force)
+    except StoreNotInitialisedError:
+        click.echo('You need to call {} init first.'.format(__name__))
+    except FileNotFoundError:
+        click.echo('{} is not in the password store.'.format(old_path))
+
+
+@cli.command()
+@click.pass_context
+def git(ctx):
     pass
+
 
 
 if __name__ == '__main__':

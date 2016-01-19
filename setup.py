cf3f3df0fd1cad30e6b85e7b2a4e9c5be9db3a19
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen@gmail.com>
2016-01-19 15:14:36 +0100

    setup: Add initial draft

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='pypass',
+    version='1.0',
+    description='ZX2C4\'s pass compatible Python library and cli',
+    author='Benedikt Rascher-Friesenhausen',
+    author_email='benediktrascherfriesenhausen+pypass@gmail.com',
+    maintainer='Benedikt Rascher-Friesenhausen',
+    maintainer_email='benediktrascherfriesenhausen+pypass@gmail.com',
+    packages=['pypass']
+)

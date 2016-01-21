3e9c03c3413b60129266875c7ef3d6fd5fcb0543
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-01-21 16:40:16 +0100

    main: Don't make editor a list
    We need to pass a list of parameters to subprocess.run.  For better
    readability and understanding, only make editor to a list in the the
    call to subprocess.run, not before.

--- pypass/__main__.py
+++ pypass/__main__.py
@@ -312,15 +312,13 @@ def edit(ctx, pass_name):
 
     editor = DEFAULT_EDITOR
     if 'EDITOR' in os.environ:
-        # subprocess.run needs flags and the like to be separate list
-        # entries.
-        editor = os.environ['EDITOR'].split()
+        editor = os.environ['EDITOR']
 
     (tmp_file, path) = tempfile.mkstemp()
     os.write(tmp_file, bytes(data, 'utf'))
     os.close(tmp_file)
 
-    res = subprocess.run(editor + [path])
+    res = subprocess.run(editor.split() + [path])
     if res.returncode != 0:
         click.echo('Password unchanged.')
         return 1

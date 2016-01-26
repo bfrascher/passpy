d76632546902d3b597c7165f8fc940749968c1e9
Benedikt Rascher-Friesenhausen <benediktrascherfriesenhausen+github@gmail.com>
2016-01-26 15:21:20 +0100

    tests: Add test for _get_gpg_recipients
    All other gpg tests are a little harder, since there is no easy way to
    determine the GPG ID(s) used to encrypt the file.

---
+++ tests/test_gpg.py
@@ -0,0 +1,39 @@
+import pytest
+
+from passpy.gpg import (
+    _get_gpg_recipients,
+    reencrypt_path,
+)
+
+
+@pytest.fixture(scope='session')
+def storedir(tmpdir_factory):
+    store = tmpdir_factory.mktemp('store')
+    gpg_id1 = store.join('.gpg-id')
+    gpg_id2 = store.join('noid', 'newid', '.gpg-id')
+    gpg_id12 = store.join('newid', '.gpg-id')
+
+    gpg_id1.write('passpy_id1\n', ensure=True)
+    gpg_id2.write('passpy_id2\n', ensure=True)
+    gpg_id12.write('passpy_id1\npasspy_id2\n', ensure=True)
+
+    return store
+
+
+def test_get_gpg_recipients(storedir):
+    recipients = _get_gpg_recipients(str(storedir))
+    assert len(recipients) == 1
+    assert recipients[0] == 'passpy_id1'
+
+    recipients = _get_gpg_recipients(str(storedir.join('noid', 'somefile')))
+    assert len(recipients) == 1
+    assert recipients[0] == 'passpy_id1'
+
+    recipients = _get_gpg_recipients(str(storedir.join('noid', 'newid')))
+    assert len(recipients) == 1
+    assert recipients[0] == 'passpy_id2'
+
+    recipients = _get_gpg_recipients(str(storedir.join('newid')))
+    assert len(recipients) == 2
+    assert recipients[0] == 'passpy_id1'
+    assert recipients[1] == 'passpy_id2'
